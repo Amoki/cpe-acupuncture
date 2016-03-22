ac970bb28bfcc97bbb45eace91b51d2742bc9a51
MargauxG <margaux.grandhomme@outlook.fr>
2016-03-22 17:15:41 +0100

    gitignore

--- templates_c/ca6c647aad79a972695a8caccb9e3246e81af28a_0.file.base.html.php
+++ templates_c/ca6c647aad79a972695a8caccb9e3246e81af28a_0.file.base.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30-dev/53, created on 2016-03-11 14:44:24
+/* Smarty version 3.1.30-dev/53, created on 2016-03-22 15:22:59
   from "/Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/base.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30-dev/53',
-  'unifunc' => 'content_56e2cbb8cbf917_00974115',
+  'unifunc' => 'content_56f15543e72274_79584130',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ca6c647aad79a972695a8caccb9e3246e81af28a' => 
     array (
       0 => '/Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/base.html',
-      1 => 1457703861,
+      1 => 1458656516,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_56e2cbb8cbf917_00974115 (Smarty_Internal_Template $_smarty_tpl) {
+function content_56f15543e72274_79584130 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->ext->_inheritance->init($_smarty_tpl, false);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <title><?php 
-new Block_titre_105890583756e2cbb8cbc3d1_48752323($_smarty_tpl);
+new Block_titre_91760227556f15543e63352_17967289($_smarty_tpl);
 ?>
 </title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -53,7 +53,7 @@ new Block_titre_105890583756e2cbb8cbc3d1_48752323($_smarty_tpl);
   <div class="container">
     <div class="row">
       <div id="navigation" class="col-sm-9 col-md-9">
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-default" role="navigation">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="container">
             <div class="navbar-header">
@@ -68,10 +68,10 @@ new Block_titre_105890583756e2cbb8cbc3d1_48752323($_smarty_tpl);
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li><a href="/" title="Accueil">Accueil</a></li>
-                <li><a href="/" title="Consultation des pathologies">Consultation </a></li>
-                <li><a href="/" title="Recherche avancée de pathologies">Recherche avancée </a></li>
-                <li><a href="/" title="Informations">Informations</a></li> 
+                <li><a role="link" href="/" accesskey="1" title="Accueil">Accueil</a></li>
+                <li><a role="link" href="" accesskey="5" title="Consultation des pathologies">Consultation </a></li>
+                <li><a role="link" href="/" accesskey="4" title="Recherche avancée de pathologies">Recherche avancée </a></li>
+                <li><a role="link" href="/" accesskey="9" title="Informations">Informations</a></li> 
               </ul>
 
             </div><!-- /.navbar-collapse -->
@@ -79,9 +79,14 @@ new Block_titre_105890583756e2cbb8cbc3d1_48752323($_smarty_tpl);
         </nav>
       </div>
       <div id="user_info" class="container col-sm-3 col-md-3">
+        <?php if ($_smarty_tpl->tpl_vars['logged']->value) {?>
         <img id="avatar" src=""/>
-        <a id="account" href="">Pseudo</a>
-        <a id="disconnect" href=""><img id="disconnect_img"/>...</a>
+        <a role="link" id="account" href=""><?php echo $_smarty_tpl->tpl_vars['username']->value;?>
+</a><br/>
+        <a role="link" id="disconnect" href="deconnexion.php"><img id="disconnect_img"/>deconnexion</a>
+        <?php } else { ?>
+          <a role="link"href='index.php'>s'inscrire / se connecter</a>
+        <?php }?>
       </div>
     </div><!-- /.container-fluid -->
 
@@ -90,7 +95,7 @@ new Block_titre_105890583756e2cbb8cbc3d1_48752323($_smarty_tpl);
     <div class="row"> <!-- "body" -->
 
     	<?php 
-new Block_content_100549609956e2cbb8cbe881_95362090($_smarty_tpl);
+new Block_content_164699878456f15543e71284_15594830($_smarty_tpl);
 ?>
 
 
@@ -100,19 +105,19 @@ new Block_content_100549609956e2cbb8cbe881_95362090($_smarty_tpl);
 </body>
 </html><?php }
 /* {block 'titre'} /Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/base.html */
-class Block_titre_105890583756e2cbb8cbc3d1_48752323 extends Smarty_Internal_Block
+class Block_titre_91760227556f15543e63352_17967289 extends Smarty_Internal_Block
 {
 public $name = 'titre';
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->ext->_inheritance->blockNesting++;
 ?>
-Acupuncture<?php
+Acupuncture et médecine chinoise<?php
 $_smarty_tpl->ext->_inheritance->blockNesting--;
 }
 }
 /* {/block 'titre'} */
 /* {block 'content'} /Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/base.html */
-class Block_content_100549609956e2cbb8cbe881_95362090 extends Smarty_Internal_Block
+class Block_content_164699878456f15543e71284_15594830 extends Smarty_Internal_Block
 {
 public $name = 'content';
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
--- templates_c/fcb554ad7cb306e19aeeb057e487a5bed32920f9_0.file.index.html.php
+++ templates_c/fcb554ad7cb306e19aeeb057e487a5bed32920f9_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30-dev/53, created on 2016-03-11 14:10:30
+/* Smarty version 3.1.30-dev/53, created on 2016-03-22 14:48:25
   from "/Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/index.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30-dev/53',
-  'unifunc' => 'content_56e2c3c6413f71_41320394',
+  'unifunc' => 'content_56f14d2997ff78_85878742',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'fcb554ad7cb306e19aeeb057e487a5bed32920f9' => 
     array (
       0 => '/Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/index.html',
-      1 => 1457701828,
+      1 => 1458654503,
       2 => 'file',
     ),
   ),
@@ -21,21 +21,21 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:pages/base.html' => 1,
   ),
 ),false)) {
-function content_56e2c3c6413f71_41320394 (Smarty_Internal_Template $_smarty_tpl) {
+function content_56f14d2997ff78_85878742 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->ext->_inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-new Block_title_165735421156e2c3c640ffe9_89839538($_smarty_tpl);
+new Block_title_169150517156f14d2997d316_60875829($_smarty_tpl);
 ?>
 
 <?php 
-new Block_content_164219092756e2c3c6412e02_57961497($_smarty_tpl);
+new Block_content_1215226256f14d2997f597_80110726($_smarty_tpl);
 $_smarty_tpl->ext->_inheritance->endChild($_smarty_tpl);
 $_smarty_tpl->_subTemplateRender("file:pages/base.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'title'} /Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/index.html */
-class Block_title_165735421156e2c3c640ffe9_89839538 extends Smarty_Internal_Block
+class Block_title_169150517156f14d2997d316_60875829 extends Smarty_Internal_Block
 {
 public $name = 'title';
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -47,7 +47,7 @@ $_smarty_tpl->ext->_inheritance->blockNesting--;
 }
 /* {/block 'title'} */
 /* {block 'content'} /Users/Margaux/Documents/CPE/TLI/cpe-acupuncture/pages/index.html */
-class Block_content_164219092756e2c3c6412e02_57961497 extends Smarty_Internal_Block
+class Block_content_1215226256f14d2997f597_80110726 extends Smarty_Internal_Block
 {
 public $name = 'content';
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -56,64 +56,70 @@ $_smarty_tpl->ext->_inheritance->blockNesting++;
 
 <div class="col-md-8 col-sm-12"> <!-- Formulaires -->
   <div class="jumbotron text-center">
-    <h2>Créer un compte</h2>
-    <form id="form_create_account" action="" method="post">
+    <h2 role="heading" aria-level=2>Créer un compte</h2>
+    <form id="form_create_account" role="form" action="inscription.php" method="post">
       <div class="form-group">
         <div class="row">
-          <div class="col-md-6 col-sm-12">
-            <div class="row">
-              <label for="Lastname">Nom&nbsp;:</label>
-              <input type="text" id="Lastname" class="form-control" required>
-            </div>
-            <div class="row">
-              <label for="e-mail">E-mail&nbsp;:</label>
-              <input type="email" id="e-mail" class="form-control" required>
-            </div>
-            <div class="row">
-              <label for="password">Mot de passe&nbsp;:</label>
-              <input type="password" id="password" class="form-control" required>
-            </div>
-          </div>
-          <div class="col-md-6 col-sm-12">
-            <div class="row">
-              <label for="name">Prénom&nbsp;:</label>
-              <input type="text" id="name" class="form-control" required>
-            </div>
-            <div class="row">
-              <label for="e-mail_confirm">Confirmation e-mail&nbsp;:</label>
-              <input type="email" id="mail_confirm" class="form-control" required>
-            </div>
-            <div class="row">
-              <label for="password_confirm">Confirmation mot de passe&nbsp;:</label>
-              <input type="password" id="password_confirm" class="form-control" required>
-            </div>
-          </div>
-        </div>
-        <input type="submit" id="create_account" class="btn btn-default" value="Créer un compte"/>
-      </div>
-    </form>
 
-    <h2>S'identifer</h2>
-    <form id="form_identify" action="" method="post">
-      <div class="form-group">
+          <div class="row"> 
+            <div class="col-md-6 col-sm-12">
+              <label for="lastname">Nom&nbsp;:</label>
+              <input type="text" role="textbox" id="lastname" class="form-control" name="lastname" placeholder="Nom"required aria-required="true">
+            </div>
+            <div class="col-md-6 col-sm-12">
+              <label for="name">Prénom&nbsp;:</label>
+              <input type="text" role="textbox" id="name" class="form-control" name="name" placeholder="Prénom" required aria-required="true">
+            </div>
+          </div>
+          <div class="row">
+           <div class="col-md-6 col-sm-12">
+            <label for="e-mail">E-mail&nbsp;:</label>
+            <input type="email" role="textbox" id="e-mail" class="form-control" name="mail" placeholder="E-mail" required aria-required="true">
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <label for="e-mail_confirm">Confirmation e-mail&nbsp;:</label>
+            <input type="email" role="textbox" id="mail_confirm" class="form-control" placeholder="Confimation e-mail" required aria-required="true" aria-required="true">
+          </div>
+        </div>
         <div class="row">
-          <div class="col-md-6 col-sm-12">
-            <div class="row">
-              <label for="e-mail_loggin">E-mail&nbsp;:</label>
-              <input type="email" id="e-mail_loggin" class="form-control" required>
-            </div>
-          </div>
-          <div class="col-md-6 col-sm-12">
-            <div class="row">
-              <label for="psw_loggin">Mot de passe&nbsp;:</label>
-              <input type="password" id="psw_loggin" class="form-control" required>
-            </div>
-          </div>
-          <input type="submit" id="identify" class="btn btn-default" value="S'identifier"/>
+         <div class="col-md-6 col-sm-12">
+          <label for="password">Mot de passe&nbsp;:</label>
+          <input type="password" role="textbox" id="password" class="form-control" name="mdp" placeholder="Mot de passe" required aria-required="true">
+        </div>
+        
+        <div class="col-md-6 col-sm-12">
+          <label for="password_confirm">Confirmation mot de passe&nbsp;:</label>
+          <input role="textbox" type="password" id="password_confirm" class="form-control" placeholder="Confirmation mot de passe" required aria-required="true">
         </div>
       </div>
-    </form>
-  </div>
+      <div class="row">
+       <input role="Submit" type="submit" id="create_account" class="btn btn-default" value="Créer un compte"/>
+     </div>
+   </div>
+ </form>
+ <div class="row space">
+ </div>
+ <h2 role="heading" aria-level=2 >S'identifer</h2>
+ <form id="form_identify" role="form" action="connexion.php" method="post">
+  <div class="form-group">
+    <div class="row">
+      <div class="row">
+        <div class="col-md-6 col-sm-12">
+
+          <label for="e-mail_loggin">E-mail&nbsp;:</label>
+          <input type="email" role="textbox" id="e-mail_loggin" class="form-control" name="email" placeholder="E-mail" required aria-required="true">
+          </div>
+        <div class="col-md-6 col-sm-12">
+          <label for="psw_loggin">Mot de passe&nbsp;:</label>
+          <input type="password" role="textbox" id="psw_loggin" class="form-control" name="mdp" placeholder="Mot de passe" required aria-required="true">
+        </div>
+        <div class="row">
+          <input role="Submit" type="submit" id="identify" class="btn btn-default" value="S'identifier"/>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
 </div> <!-- Fin formulaires -->
 
 <div class="col-md-4 col-sm-12"> <!-- Flux RSS -->
