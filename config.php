90f24b4a5f4c0bb3d7ff148da725b096b8812be5
dandray <dandray@MacBook-Air-de-Dan.local>
2016-03-24 14:47:26 +0100

    Ajout cadre flux rss

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	define('_PATH_', $_SERVER['DOCUMENT_ROOT'].'/cpe-acupuncture/');
+
+
+	$mysql_user = 'root';
+	$mysql_password = 'root';
+	$mysql_url = '127.0.0.1';
+	$mysql_database = 'acu';
+
+	$mysql_string = "mysql:host=$mysql_url;dbname=$mysql_database";
+	
+?>
+
