6e2a027bb868f60b69a05ab7fdf7a1b251560ae6
dandray <dandray@MacBook-Air-de-Dan.local>
2016-04-27 15:44:27 +0200

    verif identicté mail & mdp

--- controleur/inscription.php
+++ controleur/inscription.php
@@ -1,18 +1,28 @@
 <?php
 
-	/*Connexion à la BDD */
-	global $bdd;
-	global $smarty;
-	$smarty->assign('fail', false);
-	include(_PATH_.'config.php');
+/*Connexion à la BDD */
+global $bdd;
+global $smarty;
+$smarty->assign('fail', false);
+include(_PATH_.'config.php');
 
-	/* Récupération des données depuis le formulaire d'inscription */
-	$email = $_POST['mail'];
-	$mdp = $_POST['mdp'];
-	$nom = $_POST['lastname'];
-	$prenom = $_POST['name'];
-	
+/* Récupération des données depuis le formulaire d'inscription */
+$email = $_POST['mail'];
+$mdp = $_POST['mdp'];
+$nom = $_POST['lastname'];
+$prenom = $_POST['name'];
+$email_confirm = $_POST['e-mail_confirm'];
+$mdp_confirm = $_POST['password_confirm'];
 
+if($email != $email_confirm){
+	$smarty->assign('fail', 'Les adresses mails que vous avez saisies sont différentes. Veuillez recommencer.');
+	$_GET['page'] = 'index';
+}
+else if($mdp != $mdp_confirm){
+	$smarty->assign('fail', 'Les mots de passe que vous avez saisis sont différents. Veuillez recommencer.');
+	$_GET['page'] = 'index';
+}
+else {
 	/* On teste l'existence de l'adresse mail dans la base de données */
 	$query_test = "SELECT * FROM membre WHERE email='$email'";
 	//echo $query_test.'<br/>';
@@ -20,19 +30,19 @@
 	$result->execute();
 
 	if ($result ->rowCount() > 0) {
-    	$smarty->assign('fail', 'Cette adresse mail est déjà utilisée');
+		$smarty->assign('fail', 'Cette adresse mail est déjà utilisée');
 	} 
 	else {
-	    $hashed_password = password_hash($mdp, PASSWORD_BCRYPT, array(
-	        'salt' => $salt, 
-	    ));
-	    $query = 'INSERT INTO membre(nom, prenom, email, mdp) VALUES("'.$nom.'", "'.$prenom.'", "'.$email.'", "'.$hashed_password.'")';
-	    $req = $bdd->exec($query);
+		$hashed_password = password_hash($mdp, PASSWORD_BCRYPT, array(
+			'salt' => $salt, 
+			));
+		$query = 'INSERT INTO membre(nom, prenom, email, mdp) VALUES("'.$nom.'", "'.$prenom.'", "'.$email.'", "'.$hashed_password.'")';
+		$req = $bdd->exec($query);
 
 		//echo $query;
 		if(! $req){
-			 $smarty->assign('fail', 'Une erreur inconnue est survenue. Veuillez réessayer');
-			 $_GET['page'] = 'index';
+			$smarty->assign('fail', 'Une erreur inconnue est survenue. Veuillez réessayer');
+			$_GET['page'] = 'index';
 		}
 		else{
 			// Vérification des identifiants
@@ -48,11 +58,14 @@
 		}
 	}
 
+}
+
+
 	// Vérification de la validité des informations
 
 	// Hachage du mot de passe
 	//$pass_hache = sha1($_POST['password']);
 
 	// Insertion
-	
+
 ?>
